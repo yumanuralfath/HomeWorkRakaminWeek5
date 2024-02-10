396fb65c5c9fc89333b057d800e2cb46fa57c1a8
Yumana <yumanuralfath2@gmail.com>
2024-02-10 08:03:01 +0700

    First init

---
+++ HWWeek5.js
@@ -0,0 +1,15 @@
+//Soal
+// Buatlah 1 buah halaman yang berisi form dan tabel. Form dan tabel terpisah menjadi 2 buah tab dengan nama tab Registrasi dan
+// tab List Pendaftar. Form memiliki isian:
+// - Nama minimal 10 karakter
+// - Umur minimal 25 tahun
+// - Uang sangu minimal 100 ribu dan maksimal 1 juta
+// Setiap melakukan submit akan dicek berdasarkan kriteria tersebut, data tidak akan bisa tersimpan jika kriteria masih salah.
+// Pada tab List pendaftar akan berisi tabel yang berisi pendaftar dan resume dibawahnya, isi resume seperti berikut
+// Rata rata pendaftar memiliki uang sangu sebesar [nilai rata rata uang sangu] dengan rata rata umur [nilai rata rata umur]
+// Catatan :
+// - Imajinasikan dan coba terapkan salah satu konsep OOP dan Asynchronous dalam menyelesaikan masalah tersebut
+// - Push ke REPO dan upload link REPO, tolong repo dalam status public
+// - Gunakan framework CSS untuk membuat tampilan lebih menarik
+
+
